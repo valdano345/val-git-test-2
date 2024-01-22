7f60e03d0c00b35790f9272de96bcac007c2772a
valdano345 <valdanoesnaidar@gmail.com>
2024-01-22 15:19:21 +0700

    menambaahkan pargrahph

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remot 2</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dicta, quas ipsam velit aspernatur atque enim pariatur quam, est sed libero doloremque assumenda ipsa quia, iure consequatur cupiditate suscipit mollitia.</p>
+</body>
+</html>
